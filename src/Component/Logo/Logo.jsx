f4698f9e7c304ceea91edc47a6f0e9beeae7b219
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-08-26 12:14:23 +0600

    logo design

--- src/Component/Logo/Logo.jsx
+++ src/Component/Logo/Logo.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from '../../assets/logo.png'
 const Logo = () => {
     return (
-        <div>
+        <div className="flex items-end">
             <img src={logo} alt="Logo" />
+            <h3 className="text-3xl -ms-2.5 font-bold">FoodMove</h3>
         </div>
     );
 };
