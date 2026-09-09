4365e05a0776b43554af53d03e68da0fb6fb3713
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-09-10 01:43:24 +0600

    search flyTo location

--- src/Component/allPages/Coverage/Coverage.jsx
+++ src/Component/allPages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useLoaderData } from 'react-router';
@@ -6,12 +6,38 @@ import { useLoaderData } from 'react-router';
 const Coverage = () => {
     const position = [23.6850, 90.3563]
     const serviceCenters = useLoaderData();
-    console.log(serviceCenters);
+    const mapRef=useRef(null);
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const location = event.target.location.value;
+        const district=serviceCenters.find(c=>c.district.toLowerCase().
+        includes(location.toLowerCase()));
+        if(district){
+            const coord=[district.latitude, district.longitude];
+            mapRef.current.flyTo(coord,14);
+        }
+    };
     return (
         <div className='max-w-7xl mx-auto my-10'>
             <h3 className="text-4xl font-semibold">We are available 64 districts</h3>
-        <div>
-
+        <div className='my-5'>
+           <form onSubmit={handleSearch}>
+             <label className="input">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input type="search" className="grow" placeholder="Search" name="location" />
+            </label>
+           </form>
         </div>
 
         <div className='w-full   h-[800px] my-10'>
@@ -19,6 +45,8 @@ const Coverage = () => {
          zoom={8} 
          scrollWheelZoom={false}
          className='w-full h-full rounded-md'
+         ref={mapRef}
+         
          >
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
